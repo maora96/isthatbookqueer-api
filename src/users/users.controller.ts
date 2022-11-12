5ed5dfa805ea972222a4500cf9abbd0b3db5e6c1
Marina Oliveira Rocha <marina.oliveira@cubos.academy>
2022-11-12 20:25:44 -0300

    feat: signup and signin

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,21 +1,21 @@
-import {
-  Body,
-  Controller,
-  Post,
-  UseInterceptors,
-  ClassSerializerInterceptor,
-} from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { CreateUserDTO } from './dtos/create-user.dto';
+import { UserDTO } from './dtos/user.dto';
 import { UsersService } from './users.service';
 
+@Serialize(UserDTO)
 @Controller('auth')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Post('signup')
   createUser(@Body() body: CreateUserDTO) {
-    this.usersService.create(body.email, body.password);
+    return this.usersService.signup(body.email, body.password);
   }
 
-  // @UseInterceptors(ClassSerializerInterceptor)
+  @Post('signin')
+  signin(@Body() body: CreateUserDTO) {
+    return this.usersService.signin(body.email, body.password);
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,15 @@
 import {
-  ForbiddenException,
+  BadRequestException,
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { randomBytes, scrypt as _script } from 'crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify(_script);
 
 @Injectable()
 export class UsersService {
@@ -13,15 +17,44 @@ export class UsersService {
     @InjectRepository(User) private usersRepository: Repository<User>,
   ) {}
 
-  create(email: string, password: string) {
-    const existingUser = this.usersRepository.findOne({ where: { email } });
+  async signup(email: string, password: string) {
+    const existingUser = await this.usersRepository.findOne({
+      where: { email },
+    });
 
     if (existingUser) {
-      throw new ForbiddenException('User with this email already exists.');
+      throw new BadRequestException('User with this email already exists.');
     }
-    const user = this.usersRepository.create({ email, password });
+
+    const salt = randomBytes(8).toString('hex');
+
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    const result = salt + '.' + hash.toString('hex');
+
+    const user = this.usersRepository.create({ email, password: result });
 
     return this.usersRepository.save(user);
+  }
+
+  async signin(email: string, password: string) {
+    const existingUser = await this.usersRepository.findOne({
+      where: { email },
+    });
+
+    if (!existingUser) {
+      throw new NotFoundException('User with this email already exists.');
+    }
+
+    const [salt, storedHash] = existingUser.password.split('.');
+
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    if (storedHash !== hash.toString('hex')) {
+      throw new BadRequestException('Email or password incorrect.');
+    }
+
+    return existingUser;
   }
 
   // async update(id: number, attrs: Partial<User>) {
