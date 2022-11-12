0a4a406e481aaa309adb0b49d1d48bade6e6136c
Marina Oliveira Rocha <marina.oliveira@cubos.academy>
2022-11-12 18:14:37 -0300

    feat: create user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UseInterceptors,
+  ClassSerializerInterceptor,
+} from '@nestjs/common';
+import { CreateUserDTO } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post('signup')
+  createUser(@Body() body: CreateUserDTO) {
+    this.usersService.create(body.email, body.password);
+  }
+
+  // @UseInterceptors(ClassSerializerInterceptor)
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
+
+  create(email: string, password: string) {
+    const existingUser = this.usersRepository.findOne({ where: { email } });
+
+    if (existingUser) {
+      throw new ForbiddenException('User with this email already exists.');
+    }
+    const user = this.usersRepository.create({ email, password });
+
+    return this.usersRepository.save(user);
+  }
+
+  // async update(id: number, attrs: Partial<User>) {
+  //   const existingUser = await this.usersRepository.findOne({ where: { id } });
+
+  //   if (!existingUser) {
+  //     throw new NotFoundException('User not found.');
+  //   }
+
+  //   Object.assign(existingUser, attrs);
+
+  //   return this.usersRepository.save(existingUser);
+  // }
+
+  // async remove(id: number) {
+  //   const existingUser = await this.usersRepository.findOne({ where: { id } });
+
+  //   if (!existingUser) {
+  //     throw new NotFoundException('User not found.');
+  //   }
+
+  //   return this.usersRepository.remove(existingUser);
+  // }
+}
